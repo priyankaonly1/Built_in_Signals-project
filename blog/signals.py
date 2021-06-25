1d96adf0185e0504a55cb736e0cd3d9327b4fd92
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-06-25 23:22:58 +0530

    Test_Signals

--- blog/signals.py
+++ blog/signals.py
@@ -2,7 +2,7 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out, user_lo
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import pre_init, pre_save, pre_delete, post_init, post_save, post_delete, pre_migrate, post_migrate
-
+from django.db.backends.signals import connection_created
 from django.core.signals import request_started, request_finished, got_request_exception
 
 
@@ -151,4 +151,14 @@ def at_end_migrate_flush(sender, app_config, verbosity, interactive, using, plan
     print('Using:', using)
     print('Plan:', plan)
     print('App:', apps)
+    print(f'kwargs:{kwargs}')
+
+# Test Signals
+
+@receiver(connection_created)
+def conn_db(sender, connection, **kwargs):
+    print('----------------------------------------------------------------')
+    print('Initial connection to the database......')
+    print('Sender:', sender)
+    print('Connection:', connection)
     print(f'kwargs:{kwargs}')
