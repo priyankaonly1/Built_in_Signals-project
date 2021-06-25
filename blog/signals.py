584cfe55ca9fe21e2be4e82ef6f3267ffa91ac1f
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-06-25 23:03:07 +0530

    Request/Response Signals

--- blog/signals.py
+++ blog/signals.py
@@ -2,7 +2,10 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out, user_lo
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import pre_init, pre_save, pre_delete, post_init, post_save, post_delete
+from django.core.signals import request_started, request_finished, got_request_exception
 
+
+#  login/logout signals
 @receiver(user_logged_in,sender=User)
 def login_success(sender, request, user, **kwargs):
     print("------------------------------------------------------")
@@ -30,6 +33,9 @@ def login_failed(sender, credentials, request, **kwargs):
     print('Credentials:', credentials)
     print('Request:', request)
     print(f'kwargs:{kwargs}')
+
+
+# model signals
 
 @receiver(pre_save, sender=User)
 def at_beginning_save(sender, instance, **kwargs):
@@ -91,3 +97,28 @@ def at_ending_init(sender, *args, **kwargs):
     print(f'Args:{args}')
     print(f'kwargs:{kwargs}')
 
+# request/response signals
+
+@receiver(request_started)
+def at_beginning_request(sender, environ, **kwargs):
+    print('--------------------------------------------------------')
+    print('At Beginning Request....')
+    print('sender:', sender)
+    print('Environ:', environ)
+    print(f'kwargs:{kwargs}')
+
+@receiver(request_finished)
+def at_ending_request(sender, **kwargs):
+    print('--------------------------------------------------------')
+    print('At Ending Request....')
+    print('sender:', sender)
+    print(f'kwargs:{kwargs}')
+
+
+@receiver(got_request_exception)
+def at_request_exception(sender, request, **kwargs):
+    print('--------------------------------------------------------')
+    print('At Request Exception....')
+    print('sender:', sender)
+    print('Request:', request)
+    print(f'kwargs:{kwargs}')
